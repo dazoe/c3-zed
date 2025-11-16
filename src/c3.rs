eead6399c61c8d732f479d94fc11dfacedac5fc9
dong <dongrunyu2023@outlook.com>
2025-11-16 10:39:37 +0800

    Update c3.rs

--- src/c3.rs
+++ src/c3.rs
@@ -1,9 +1,39 @@
+use std::{
+    fs::{self, File},
+    io::{Error, ErrorKind, Read, Write},
+};
+
 use zed_extension_api::{
     self as zed, current_platform, download_file, latest_github_release, make_file_executable,
     Architecture, GithubReleaseOptions, Os, Result,
 };
 
 struct C3Extension;
+
+impl C3Extension {
+    fn download_lsp(release: &zed::GithubRelease) {
+        {
+            if let Ok(_) = match current_platform() {
+                (Os::Windows, Architecture::X8664) => download_file(
+                    &release.assets[2].download_url,
+                    "c3lsp/",
+                    zed_extension_api::DownloadedFileType::Zip,
+                ),
+                (Os::Mac, Architecture::Aarch64) => download_file(
+                    &release.assets[0].download_url,
+                    "c3lsp/",
+                    zed_extension_api::DownloadedFileType::Zip,
+                ),
+                (Os::Linux, Architecture::X8664) => download_file(
+                    &release.assets[1].download_url,
+                    "c3lsp/",
+                    zed_extension_api::DownloadedFileType::GzipTar,
+                ),
+                _ => Err("no available lsp!".to_string()),
+            } {}
+        }
+    }
+}
 
 impl zed::Extension for C3Extension {
     fn new() -> Self {
@@ -22,24 +52,22 @@ impl zed::Extension for C3Extension {
                 require_assets: false,
             },
         ) {
-            if let Ok(_) = match current_platform() {
-                (Os::Windows, Architecture::X8664) => download_file(
-                    &release.assets[2].download_url,
-                    "c3lsp/",
-                    zed_extension_api::DownloadedFileType::Zip,
-                ),
-                (Os::Mac, Architecture::Aarch64) => download_file(
-                    &release.assets[0].download_url,
-                    "c3lsp/",
-                    zed_extension_api::DownloadedFileType::Zip,
-                ),
-                (Os::Linux, Architecture::X8664) => download_file(
-                    &release.assets[1].download_url,
-                    "c3lsp/",
-                    zed_extension_api::DownloadedFileType::Zip,
-                ),
-                _ => Err("no available lsp!".to_string()),
-            } {}
+            let mut file = match File::open("lsp_ver") {
+                Ok(file_handle) => file_handle,
+                Err(e) => match e.kind() {
+                    ErrorKind::NotFound => File::create("lsp_ver").unwrap(),
+                    _ => return Err("Failed load file".to_string()),
+                },
+            };
+            let mut content = String::new();
+
+            file.read_to_string(&mut content).unwrap_or_default();
+
+            if content != release.version {
+                fs::write("lsp_ver", release.version.as_bytes())
+                    .map_err(|_| "Failed to write file".to_string())?;
+                Self::download_lsp(&release);
+            }
         }
 
         let path = match current_platform() {
@@ -49,7 +77,7 @@ impl zed::Extension for C3Extension {
             _ => "no available lsp!",
         };
 
-        make_file_executable(path)?;
+        make_file_executable(&path)?;
 
         Ok(zed::Command {
             command: path.to_string(),
